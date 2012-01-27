7ad56134931f6e78c37d41a9826184e023ac6319
Ibrahima Sylla <sorysylla@hotmail.com>
2012-01-26 21:07:05 -0500

    tache1: premier commit

---
+++ src/demo2git/Demo2Git.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo2git;
+
+/**
+ *
+ * @author sorysylla
+ */
+public class Demo2Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("allo");
+    }
+}
